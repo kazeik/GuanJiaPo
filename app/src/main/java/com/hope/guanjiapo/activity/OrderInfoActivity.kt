cc6a5d9ebc75d63a892dc5d4d4031d20feed0abd
Dector <chenkai851206>
2019-02-27 14:34:01 +0800

    修改运单详情的连续关系

--- app/src/main/java/com/hope/guanjiapo/activity/OrderInfoActivity.kt
+++ app/src/main/java/com/hope/guanjiapo/activity/OrderInfoActivity.kt
@@ -3,6 +3,7 @@ package com.hope.guanjiapo.activity
 import android.app.AlertDialog
 import android.content.Intent
 import android.view.View
+import android.widget.RadioGroup
 import com.hope.guanjiapo.R
 import com.hope.guanjiapo.base.BaseActivity
 import com.hope.guanjiapo.base.BaseModel
@@ -11,12 +12,15 @@ import com.hope.guanjiapo.model.DestinationModel
 import com.hope.guanjiapo.net.HttpNetUtils
 import com.hope.guanjiapo.net.NetworkScheduler
 import com.hope.guanjiapo.net.ProgressSubscriber
+import com.hope.guanjiapo.utils.ApiUtils
+import com.hope.guanjiapo.utils.ApiUtils.loginModel
 import com.hope.guanjiapo.utils.ApiUtils.vehicleModel
 import kotlinx.android.synthetic.main.activity_order_info.*
 import kotlinx.android.synthetic.main.view_title.*
 import org.jetbrains.anko.startActivity
 import org.jetbrains.anko.startActivityForResult
 import org.jetbrains.anko.toast
+import org.json.JSONObject
 
 
 class OrderInfoActivity : BaseActivity(), View.OnClickListener {
@@ -27,6 +31,11 @@ class OrderInfoActivity : BaseActivity(), View.OnClickListener {
     private var fhdStr: String? = ""
     private var ccStr: String? = ""
     private var bzdwStr: String? = ""
+    private var recway: Int = 0
+    private var shipfeepaytype: Int = 0
+    private var fkStr: String = ""
+    private var hdfsStr: String = ""
+    private var tzfh: Int = 0
 
     override fun initData() {
         tvTitle.setText(R.string.orderinfo)
@@ -42,6 +51,20 @@ class OrderInfoActivity : BaseActivity(), View.OnClickListener {
         ivShr.setOnClickListener(this)
         ivGys.setOnClickListener(this)
         ivLxr.setOnClickListener(this)
+
+        mPayTypeGroup.setOnCheckedChangeListener { radioGroup: RadioGroup, i: Int ->
+            when (i) {
+                R.id.rbXf -> shipfeepaytype = 0
+                R.id.rbYj -> shipfeepaytype = 1
+                R.id.rbTf -> shipfeepaytype = 2
+            }
+        }
+        msgShfs.setOnCheckedChangeListener { radioGroup: RadioGroup, i: Int ->
+            when (i) {
+                R.id.rbZt -> recway = 0
+                R.id.rbPs -> recway = 1
+            }
+        }
     }
 
     private fun showCcListDialog() {
@@ -59,8 +82,10 @@ class OrderInfoActivity : BaseActivity(), View.OnClickListener {
         val items = resources.getStringArray(R.array.bzdw)
         val listDialog = AlertDialog.Builder(this)
         listDialog.setTitle("请选择包装单位")
-        listDialog.setItems(items, { dialog, which ->
-        })
+        listDialog.setItems(items) { dialog, which ->
+            fhdStr = items[which]
+            tvBzdw.text = fhdStr
+        }
         listDialog.show()
     }
 
@@ -68,8 +93,10 @@ class OrderInfoActivity : BaseActivity(), View.OnClickListener {
         val items = arrayOf("我是1", "我是2", "我是3", "我是4")
         val listDialog = AlertDialog.Builder(this)
         listDialog.setTitle("请选择发货点")
-        listDialog.setItems(items, { dialog, which ->
-        })
+        listDialog.setItems(items) { dialog, which ->
+            fhdStr = items[which]
+            tvFhd.text = fhdStr
+        }
         listDialog.show()
     }
 
@@ -77,7 +104,7 @@ class OrderInfoActivity : BaseActivity(), View.OnClickListener {
         when (v?.id) {
             R.id.ivBackup -> finish()
             R.id.tvTitleRight -> createOrder()
-            R.id.tvFwhy -> startActivity<PremiumActivity>()
+            R.id.tvFwhy -> startActivityForResult<PremiumActivity>(200)
             R.id.tvCc -> showCcListDialog()
             R.id.tvBzdw -> showBzdwListDialog()
             R.id.tvFhd -> showFhdListDialog()
@@ -107,7 +134,46 @@ class OrderInfoActivity : BaseActivity(), View.OnClickListener {
         val fhrStr = etFhr.text.toString()
         val bzStr = etBz.text.toString()
 
-        HttpNetUtils.getInstance().getManager()?.wladd(hashMapOf())?.compose(NetworkScheduler.compose())
+        HttpNetUtils.getInstance().getManager()?.wladd(
+            hashMapOf(
+                "id" to loginModel?.id!!,
+                "sessionId" to loginModel?.sessionid!!,
+                "mobile" to loginModel?.mobile!!,
+                "order" to JSONObject(
+                    hashMapOf(
+                        "copycount" to hdfsStr, //回单份数
+                        "agentmoney" to zzfStr, //中转费
+                        "shipfee" to 0, //运费 （总计
+                        "serviceName" to "",//供应商
+                        "dispatchfee" to psfStr, //派送费
+                        "receivername" to shrStr,//收货人
+                        "productweight" to zlStr,//重量
+                        "receivepoint" to mddStr, //目的地
+                        "shipfeesendpay" to zzfStr,//中转费
+                        "costFee" to cbStr, //成本
+                        "senderphone" to "",//发货人电话
+                        "shipfeestate" to "",//运费支付，0欠款，1已付
+                        "shipfeepaytype" to shipfeepaytype, //支付方式
+                        "sendername" to fhrStr,//发货人
+                        "receiveraddress" to "", //收货人的地址
+                        "waitnotify" to tzfh,//等通知放货，0否，1是
+                        "productno" to kddhStr,//快递单号
+                        "baseshipfee" to jbyfStr, //基本运费
+                        "insurancefee" to bfStr, //保费
+                        "recno" to 1,
+                        "productcount" to hwslStr,//货物数量
+                        "recway" to recway,//收货方式，0自提，1派送
+                        "productsize" to tjStr,//体积
+                        "receiverphone" to "",//收货人电话
+                        "productdescript" to hwmcStr, //货物名称
+                        "returnmoney" to fkStr,//返款
+                        "carname" to ccStr!!,//车次
+                        "comment" to bzStr, //备注
+                        "senderaddress" to ""//发货人地址
+                    )
+                ).toString()
+            )
+        )?.compose(NetworkScheduler.compose())
             ?.subscribe(object : ProgressSubscriber<BaseModel<String>>(this) {
                 override fun onSuccess(data: BaseModel<String>?) {
                     toast(data?.msg!!)
@@ -120,6 +186,11 @@ class OrderInfoActivity : BaseActivity(), View.OnClickListener {
         super.onActivityResult(requestCode, resultCode, data)
         if (null == data) return
         when (requestCode) {
+            200 -> {
+                fkStr = intent.getStringExtra("fk")
+                hdfsStr = intent.getStringExtra("hdfs")
+                tzfh = intent.getIntExtra("tzfh", 0)
+            }
             199 -> {
                 val item = data.getSerializableExtra("data") as DestinationModel
                 etMdd.setText(item.receivepoint)
--- app/src/main/java/com/hope/guanjiapo/activity/PremiumActivity.kt
+++ app/src/main/java/com/hope/guanjiapo/activity/PremiumActivity.kt
@@ -1,9 +1,12 @@
 package com.hope.guanjiapo.activity
 
+import android.content.Intent
 import android.view.View
+import android.widget.RadioGroup
 import com.hope.guanjiapo.R
 import com.hope.guanjiapo.base.BaseActivity
 import com.hope.guanjiapo.model.SubscribeModel
+import kotlinx.android.synthetic.main.activity_premium.*
 import kotlinx.android.synthetic.main.view_title.*
 
 class PremiumActivity : BaseActivity(), View.OnClickListener {
@@ -11,20 +14,31 @@ class PremiumActivity : BaseActivity(), View.OnClickListener {
         return R.layout.activity_premium
     }
 
-    private var subscribeModel:SubscribeModel?= null
+    private var tzfh: Int = 0
     override fun initData() {
-        subscribeModel = intent.getSerializableExtra("data") as? SubscribeModel
-
         tvTitle.setText(R.string.fwhy)
         ivBackup.setOnClickListener(this)
+        msgShfs.setOnCheckedChangeListener { radioGroup: RadioGroup, i: Int ->
+            when (i) {
+                R.id.rbNo -> tzfh = 0
+                R.id.rbSure -> tzfh = 1
+            }
+        }
     }
 
 
     override fun onClick(v: View?) {
         when (v?.id) {
-            R.id.ivBackup -> finish()
-            R.id.tvTitleRight -> {}
-            R.id.tvFwhy->{}
+            R.id.ivBackup -> {
+                val fkStr = etFk.text.toString()
+                val hdfsStr = etHdfs.text.toString()
+                val intent = Intent()
+                intent.putExtra("fk", fkStr)
+                intent.putExtra("hdfs", hdfsStr)
+                intent.putExtra("tzfh", tzfh)
+                setResult(200, intent)
+                finish()
+            }
         }
     }
 
