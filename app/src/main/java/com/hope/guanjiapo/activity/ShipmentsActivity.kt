a6986ffb40149af6a6e8702a62a7710cd9becade
Dector <chenkai851206>
2019-03-01 11:42:27 +0800

    修改发货点列表的错误

--- app/src/main/java/com/hope/guanjiapo/activity/ShipmentsActivity.kt
+++ app/src/main/java/com/hope/guanjiapo/activity/ShipmentsActivity.kt
@@ -4,15 +4,17 @@ import android.app.AlertDialog
 import android.content.Intent
 import android.support.v7.widget.LinearLayoutManager
 import android.text.Editable
+import android.text.TextUtils
 import android.text.TextWatcher
 import android.view.View
+import android.widget.EditText
 import com.hope.guanjiapo.R
-import com.hope.guanjiapo.adapter.DestinationAdapter
+import com.hope.guanjiapo.adapter.ShipmentsAdapter
 import com.hope.guanjiapo.base.BaseActivity
 import com.hope.guanjiapo.base.BaseModel
 import com.hope.guanjiapo.iter.OnItemEventListener
 import com.hope.guanjiapo.iter.OnItemLongEventListener
-import com.hope.guanjiapo.model.DestinationModel
+import com.hope.guanjiapo.model.VehicleModel
 import com.hope.guanjiapo.net.HttpNetUtils
 import com.hope.guanjiapo.net.NetworkScheduler
 import com.hope.guanjiapo.net.ProgressSubscriber
@@ -20,18 +22,17 @@ import com.hope.guanjiapo.utils.ApiUtils.loginModel
 import com.hope.guanjiapo.view.RecycleViewDivider
 import kotlinx.android.synthetic.main.activity_search_recycler.*
 import kotlinx.android.synthetic.main.view_title.*
-import org.jetbrains.anko.startActivity
 import org.jetbrains.anko.toast
 
 
-class ShipmentsActivity : BaseActivity(), View.OnClickListener, OnItemEventListener,OnItemLongEventListener {
+class ShipmentsActivity : BaseActivity(), View.OnClickListener, OnItemEventListener, OnItemLongEventListener {
     override fun onItemLongEvent(pos: Int) {
         showListDialog(pos)
     }
 
     override fun onItemEvent(pos: Int) {
         val intt = Intent()
-        intt.putExtra("data", allitem?.get(pos))
+        intt.putExtra("data", allitem.get(pos))
         setResult(195, intt)
         finish()
     }
@@ -41,26 +42,71 @@ class ShipmentsActivity : BaseActivity(), View.OnClickListener, OnItemEventListe
         val listDialog = AlertDialog.Builder(this)
         listDialog.setItems(items) { _, which ->
             when (which) {
-                0 -> startActivity<AddDestinationActivity>("change" to true, "id" to allitem!![pos].id)
-                1 -> delete()
+                0 -> {
+                    val msg = allitem[pos]
+                    allitem.removeAt(pos)
+                    showInputDialog(msg)
+                }
+                1 -> delete(pos)
             }
         }
         listDialog.show()
     }
 
+    private fun showInputDialog(msg: String) {
+        val editText = EditText(this)
+        if (!TextUtils.isEmpty(msg))
+            editText.setText(msg)
+        val inputDialog = AlertDialog.Builder(this)
+        inputDialog.setTitle("请输入发货点").setView(editText)
+        inputDialog.setNegativeButton(R.string.cancel, null)
+        inputDialog.setPositiveButton(
+            R.string.sure
+        ) { dialog, _ ->
+            dialog.dismiss()
+            val car = editText.text.toString()
+            if (TextUtils.isEmpty(car)) {
+                return@setPositiveButton
+            }
+            if (allitem.contains(car)) {
+                toast("列表中已有$car")
+                return@setPositiveButton
+            }
+            var tempAllString = ""
+            allitem.add(car)
+            allitem.forEach {
+                tempAllString += "$it,"
+            }
+            tempAllString = tempAllString.substring(0, tempAllString.length - 1)
+            HttpNetUtils.getInstance().getManager()?.editCompanyInfo(
+                hashMapOf(
+                    "id" to loginModel?.id!!,
+                    "mobile" to loginModel?.mobile!!,
+                    "faHuoDiList" to tempAllString,
+                    "sessionId" to loginModel?.sessionid!!
+                )
+            )
+                ?.compose(NetworkScheduler.compose())?.subscribe(object : ProgressSubscriber<BaseModel<String>>(this) {
+                    override fun onSuccess(data: BaseModel<String>?) {
+                        adapter.setDataEntityList(allitem)
+                    }
+                })
+        }.show()
+    }
+
     override fun onClick(v: View?) {
         when (v?.id) {
             R.id.ivBackup -> finish()
-            R.id.tvTitleRight -> startActivity<AddDestinationActivity>()
+            R.id.tvTitleRight -> showInputDialog("")
         }
     }
 
-    private val adapter: DestinationAdapter<DestinationModel> by lazy { DestinationAdapter<DestinationModel>() }
+    private val adapter: ShipmentsAdapter<String> by lazy { ShipmentsAdapter<String>() }
     override fun getLayoutView(): Int {
         return R.layout.activity_search_recycler
     }
 
-    private val allitem: ArrayList<DestinationModel> by lazy{ arrayListOf<DestinationModel>()}
+    private val allitem: ArrayList<String> by lazy { arrayListOf<String>() }
     override fun initData() {
         tvTitle.setText(R.string.fhdlist)
         tvTitleRight.setText(R.string.create)
@@ -71,9 +117,10 @@ class ShipmentsActivity : BaseActivity(), View.OnClickListener, OnItemEventListe
         rcvDataList.layoutManager = LinearLayoutManager(this)
         rcvDataList.addItemDecoration(RecycleViewDivider(this, LinearLayoutManager.VERTICAL))
         rcvDataList.adapter = adapter
+        adapter.itemLongListener = this
         adapter.itemListener = this
 
-        etSearch.addTextChangedListener(object: TextWatcher {
+        etSearch.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(s: Editable?) {
             }
 
@@ -82,34 +129,44 @@ class ShipmentsActivity : BaseActivity(), View.OnClickListener, OnItemEventListe
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 val msg = etSearch.text.toString()
-                val templist = allitem.filter { it.receivepoint.contains(msg)  }
+                val templist = allitem.filter { it.contains(msg) }
                 adapter.setDataEntityList(templist)
             }
         })
 
-        HttpNetUtils.getInstance().getManager()?.getcompanyPointList(
+        HttpNetUtils.getInstance().getManager()?.getCompanyInfo(
             hashMapOf("id" to loginModel?.id!!, "sessionId" to loginModel?.sessionid!!, "type" to 1)
         )?.compose(NetworkScheduler.compose())
-            ?.subscribe(object : ProgressSubscriber<BaseModel<List<DestinationModel>>>(this) {
-                override fun onSuccess(data: BaseModel<List<DestinationModel>>?) {
+            ?.subscribe(object : ProgressSubscriber<BaseModel<VehicleModel>>(this) {
+                override fun onSuccess(data: BaseModel<VehicleModel>?) {
                     allitem.clear()
-                    allitem.addAll(data?.data!!)
-                    adapter.setDataEntityList(data.data!!)
+                    val allpoint = data?.data?.fahuodilist?.split(",")
+                    allpoint?.forEach {
+                        if (!TextUtils.isEmpty(it))
+                            allitem.add(it)
+                    }
+                    adapter.setDataEntityList(allitem)
                 }
             })
     }
 
-    private fun delete() {
-        HttpNetUtils.getInstance().getManager()?.deletecompanyPoint(
+    private fun delete(pos: Int) {
+        allitem.removeAt(pos)
+        var tempAllString = ""
+        allitem.forEach {
+            tempAllString += "$it,"
+        }
+        tempAllString.substring(0, tempAllString.length - 1)
+        HttpNetUtils.getInstance().getManager()?.editCompanyInfo(
             hashMapOf(
-                "bossid" to loginModel?.bossId!!,
-                "customerid" to 7,
+                "faHuoDiList" to tempAllString,
                 "id" to loginModel?.id!!,
                 "sessionId" to loginModel?.sessionid!!,
                 "mobile" to loginModel?.mobile!!
             )
         )?.compose(NetworkScheduler.compose())?.subscribe(object : ProgressSubscriber<BaseModel<String>>(this) {
             override fun onSuccess(data: BaseModel<String>?) {
+                adapter.setDataEntityList(allitem)
                 toast(data?.msg!!)
             }
         })
