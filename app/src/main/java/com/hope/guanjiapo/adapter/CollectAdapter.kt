e713e9a8218b63e2e779d60273a8119dd7209051
Dector <chenkai851206>
2019-04-08 13:19:04 +0800

    修正错误

--- app/src/main/java/com/hope/guanjiapo/adapter/CollectAdapter.kt
+++ app/src/main/java/com/hope/guanjiapo/adapter/CollectAdapter.kt
@@ -4,20 +4,18 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Color
 import android.view.Gravity
-import android.view.LayoutInflater
 import android.widget.LinearLayout
 import android.widget.TextView
 import com.hope.guanjiapo.R
 import com.hope.guanjiapo.base.BaseAdapter
 import com.hope.guanjiapo.base.BaseViewHolder
-import com.hope.guanjiapo.model.WaybillModel
 import org.jetbrains.anko.textColor
 import java.util.*
 
 
 /**
  * @author kazeik chen
- *         QQ:77132995 email:kazeik@163.com
+ *         QQ:77132995 email:kazeik@163.coms
  *         2019 03 12 10:53
  * 类说明:
  */
@@ -35,9 +33,9 @@ class CollectAdapter<B>(private val context: Context) : BaseAdapter<B>() {
             val textView = TextView(context)
             textView.text = it
             textView.textColor = Color.BLACK
-            textView.gravity = Gravity.CENTER
+            textView.gravity = Gravity.CENTER_HORIZONTAL or (Gravity.CENTER_VERTICAL)
             textView.textSize = 14.0f
-            textView.setPadding(5, 5, 5, 5)
+            textView.setPadding(3, 5, 3, 5)
             textView.layoutParams = LinearLayout.LayoutParams(
                 LinearLayout.LayoutParams.MATCH_PARENT,
                 LinearLayout.LayoutParams.WRAP_CONTENT,
